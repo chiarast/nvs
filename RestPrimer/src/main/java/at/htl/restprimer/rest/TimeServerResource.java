c8a023268bc88c4fece0ff3b438f536ec3fbff8d
Chiara <chiara.strutzenberger@gmail.com>
2019-10-03 12:37:01 +0200

    upload homework

---
+++ RestPrimer/src/main/java/at/htl/restprimer/rest/TimeServerResource.java
@@ -0,0 +1,13 @@
+package at.htl.restprimer.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.LocalDateTime;
+
+@Path("time")
+public class TimeServerResource {
+    @GET
+    public String time(){
+        return "Time: " + LocalDateTime.now();
+    }
+}
